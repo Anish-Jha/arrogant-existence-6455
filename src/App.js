ab8c4566b84d2b0427991870d270ccbd362bad9f
Ztch9 <anishj545@gmail.com>
2022-12-15 01:17:35 +0530

    file

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DarkModeSwitch from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+     <DarkModeSwitch/>
+    </div>
+  );
+}
+
+export default App;
